d3d9929d966cb7929e50fce0e376628e62cc92f8
UmbraFerreus <mzhukov31415dev@gmail.com>
2017-09-05 12:42:49 +0300

    Fixed some Java build issues (1.6 - 1.7 - 1.8). Bumped main version to 0.1.3.

--- testapp/platforms/android/assets/www/js/index.js
+++ testapp/platforms/android/assets/www/js/index.js
@@ -21,13 +21,65 @@ var app = {
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
-
+    onClickBlinkFlash: function() {/*2x10*/
+        const blink = [25,1000,25,5000,10,50,10,50,10,50,10,50,10,50,10,50,10,50,10,50,10,50,10,50];/*an array with on/off times in ms (starts with 'on')*/
+        document.getElementById('blinkFlashLabel').style.color = '#000000';
+        document.getElementById('blinkFlashLabel').innerHTML = 'Launching plugin function blinkFlash.....';
+        document.getElementById('deviceready').querySelector('.received').innerHTML = 'Flasging.....';
+        document.getElementById('deviceready').querySelector('.received').style.color = '#000000';
+        cordova.plugins.CeruleanPlugins.blinkFlash(
+         blink, 
+         function(msg) {
+                        document.getElementById('deviceready').querySelector('.received').innerHTML = msg;
+                        document.getElementById('blinkFlashLabel').style.color = '#00AA00';
+                        document.getElementById('blinkFlashLabel').innerHTML = 'Success. blinkFlash actions\' message is:\''+msg+'\'';
+                       },
+         function(err) {
+                        document.getElementById('deviceready').querySelector('.received').innerHTML = err;
+                        document.getElementById('deviceready').querySelector('.received').style.color='#FF0000';
+                        document.getElementById('blinkFlashLabel').style.color = '#FF0000';
+                        document.getElementById('blinkFlashLabel').innerHTML = 'Error. blinkFlash actions\' message is:\''+err+'\'';
+                       }
+        );
+    },
+    onClickBlinkWatch: function(event) {
+        document.getElementById('blinkWatchLabel').style.color = '#000000';
+        document.getElementById('blinkWatchLabel').innerHTML = 'Launching plugin function blinkWatch.....';
+        event.stopImmediatePropagation();
+        document.getElementById('deviceready').querySelector('.received').innerHTML = 'Watching.....';
+        document.getElementById('deviceready').querySelector('.received').style.color = '#000000';
+        cordova.plugins.CeruleanPlugins.blinkWatch(
+         {pic:true,stats:false}, 
+         function(msg) {
+                        document.getElementById('deviceready').querySelector('.received').innerHTML = msg;
+                        document.getElementById('blinkWatchLabel').style.color = '#00AA00';
+                        document.getElementById('blinkWatchLabel').innerHTML = 'Success. blinkWatch actions\' message is:\''+msg+'\'';
+                       },
+         function(err) {
+                        document.getElementById('deviceready').querySelector('.received').innerHTML = err;
+                        document.getElementById('deviceready').querySelector('.received').style.color = '#FF0000;';
+                        document.getElementById('blinkWatchLabel').style.color = '#FF0000';
+                        document.getElementById('blinkWatchLabel').innerHTML = 'Error. blinkWatch actions\' message is:\''+err+'\'';
+                       }
+        );
+    },
     // deviceready Event Handler
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+        document.getElementById('blinkFlashButton').addEventListener('click', this.onClickBlinkFlash.bind(this), false);
+        document.getElementById('blinkFlashLabel').innerHTML = 'Click event handler calling <span class=\'strng\'>\'blinkFlash\'</span> action at service <span class=\'strng\'>\'CeruleanPlugins\'</span> is bound to blinkFlashButton...';
+        document.getElementById('blinkWatchButton').addEventListener('click', this.onClickBlinkWatch.bind(this), false);
+        document.getElementById('blinkWatchLabel').innerHTML = 'Click event handler calling <span class=\'strng\'>\'blinkWatch\'</span> action at service <span class=\'strng\'>\'CeruleanPlugins\'</span> isbound to blinkWatchButton...';
         this.receivedEvent('deviceready');
+/*
+        cordova.plugins.CeruleanPlugins.myMethod(
+         'Plugin Ready!', 
+         function(msg) {document.getElementById('deviceready').querySelector('.received').innerHTML = msg;},
+         function(err) {document.getElementById('deviceready').innerHTML = '<p class="event received">' + err + '</p>';}
+        );
+*/
     },
 
     // Update DOM on a Received Event
