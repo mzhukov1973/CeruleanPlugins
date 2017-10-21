258f1e019d909be7b4884d2bf70258ed3d082352
UmbraFerreus <mzhukov31415dev@gmail.com>
2017-10-21 22:40:28 +0300

    A general update - not much.

--- SimpleCam2PluginTest/cam2app/buildDebug.sh
+++ SimpleCam2PluginTest/cam2app/buildDebug.sh
@@ -2,10 +2,10 @@
 
 #Plugin remove-add:
 cordova plugin remove cw.plugins.cam2plug --searchpath ../cam2plug --verbose --save
-cordova plugin add cw.plugins.cam2plug@0.0.2-alpha1 --searchpath ../cam2plug --noregistry --verbose --save
+#cordova plugin add cw.plugins.cam2plug@0.0.2-alpha1 --searchpath ../cam2plug --noregistry --verbose --save
 
 #cordova build android --debug  --device -- --keystore=$mz_keyStore --storePassword=$mz_keyStorePassword --alias=$mz_keyAlias --password=$mz_keyAliasPassword
-cordova run   android --debug  --device -- --keystore=$mz_keyStore --storePassword=$mz_keyStorePassword --alias=$mz_keyAlias --password=$mz_keyAliasPassword
+#cordova run   android --debug  --device -- --keystore=$mz_keyStore --storePassword=$mz_keyStorePassword --alias=$mz_keyAlias --password=$mz_keyAliasPassword
 #cordova run   android --debug  --noprepare --nobuild --device -d
 
 rm -f ./platforms/android/*signing.properties
--- SimpleCam2PluginTest/cam2app/platforms/android/assets/www/cordova_plugins.js
+++ SimpleCam2PluginTest/cam2app/platforms/android/assets/www/cordova_plugins.js
@@ -1,19 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "cw.plugins.cam2plug.Cam2Plug",
-    "file": "plugins/cw.plugins.cam2plug/www/Cam2Plug.js",
-    "pluginId": "cw.plugins.cam2plug",
-    "clobbers": [
-      "cordova.plugins.Cam2Plug"
-    ]
-  }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.2",
-  "cw.plugins.cam2plug": "0.0.2-alpha1"
+  "cordova-plugin-whitelist": "1.3.2"
 };
 // BOTTOM OF METADATA
 });
